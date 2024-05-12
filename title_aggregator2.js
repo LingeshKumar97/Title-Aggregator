5059f9f78d12213148fafb9a745296c28a0ad966
Lingesh Kumar <88303237+LingeshKumar97@users.noreply.github.com>
2024-05-12 23:07:34 +0800

    Update title_aggregator2.js

--- title_aggregator2.js
+++ title_aggregator2.js
@@ -4,7 +4,6 @@ const { RequestQueue, CheerioCrawler, Dataset }  = require('crawlee');
 const scrapWebsite = async () => {
     const requestQueue = await RequestQueue.open();
 
-    // Add requests for multiple pages
     for (let x = 1; x < 349; x++) {
         await requestQueue.addRequest({ url: `https://sea.mashable.com/?page=${x}&ist=broll` });
         console.log("page", x);
@@ -19,7 +18,7 @@ const scrapWebsite = async () => {
             $('.blogroll').each((index, element) => {
                 const caption = $(element).find('.caption').text().trim();
                 const time = $(element).find('.datepublished').text().trim();
-                const hyperlink = $(element).find('a').attr('href'); // Find <a> tag and get the href attribute
+                const hyperlink = $(element).find('a').attr('href'); 
                 if (new Date(time) >= new Date('2021-12-31')) {
                     page1.push({ title: caption, date: time, hyperlink: hyperlink });
                 }
@@ -37,7 +36,6 @@ const generateHTML = (pageData) => {
     let htmlContent = '<!DOCTYPE html><html><head><title>Scraped Data</title><style> .item { margin-bottom: 20px; } </style></head><body>';
 
     pageData.forEach(item => {
-        // Encode the URL to ensure it's correctly formatted for HTML
         const encodedUrl = encodeURI(item.hyperlink);
         htmlContent += `<div class="item"><h2><a href="${encodedUrl}">${item.title}</a></h2><p>Date: ${item.date}</p></div>`;
     });
